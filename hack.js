5f46106e72fc71edba33271cb560683ade9cd20b
정승기 <GIrurGI1028@gmail.com>
2021-12-23 13:36:52 +0900

    Update hack.js
    옵션 만들었음

--- hack.js
+++ hack.js
@@ -1,30 +1,37 @@
+let kkutuHack = {};
+
 (async () => {
   const response = await fetch(
     "https://raw.githubusercontent.com/DwarfSGJ/kkutuKoreaHack/stable/list.json"
   );
   const text = await response.text();
-
   const list = JSON.parse(text);
   let historyList = [];
+  let option = { sort: false, endWord: "" };
 
   function getStartWord() {
     return document.getElementsByClassName("jjo-display ellipse")[0].innerText;
   }
 
-  function getWord(startWord, endWord = "") {
+  function getWord(startWord, endWord, lwo) {
     if (startWord.includes("(")) {
       startWord = startWord.replace(")", "").split("(");
-      return getWord(startWord[0], endWord)
+      const wordList = getWord(startWord[0], endWord)
         .concat(getWord(startWord[1], endWord))
         .filter((e) => e !== undefined);
+      if (lwo) wordList.sort((a, b) => b.length - a.length);
+      if (endWord !== "")
+        wordList.sort((a, b) => (a.endsWith(endWord) ? 1 : -1));
+      return wordList;
     }
 
-    return list.filter(
-      (e) =>
-        !historyList.includes(e) &&
-        e.startsWith(startWord) &&
-        e.endsWith(endWord)
+    const wordList = list.filter(
+      (e) => !historyList.includes(e) && e.startsWith(startWord)
     );
+
+    if (lwo) wordList.sort((a, b) => b.length - a.length);
+    if (endWord !== "") wordList.sort((a, b) => (a.endsWith(endWord) ? 1 : -1));
+    return wordList;
   }
 
   function updateHistory() {
@@ -45,7 +52,10 @@
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       if (mutation.target.style.display === "block") {
-        console.log(getStartWord() + ": " + getWord(getStartWord()).join("\n"));
+        const startWord = getStartWord();
+        const wordList = getWord(startWord, option.endWord, option.sort);
+
+        console.log(startWord + ": " + wordList.join("\n"));
       }
     });
   });
@@ -55,5 +65,14 @@
     .getElementsByClassName("game-input")[0];
   observer.observe(target, { attributes: true, attributeFilter: ["style"] });
 
-  setInterval(updateHistory, 100);
+  const task = setInterval(updateHistory, 100);
+
+  kkutuHack = {
+    list,
+    getStartWord,
+    getWord,
+    option,
+    observer,
+    task,
+  };
 })();
